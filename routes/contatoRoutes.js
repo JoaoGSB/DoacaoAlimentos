2070fdbe5eb10d0430c5f2e7160913aad7abdd29
JoaoGSB <fwbassaco82@gmail.com>
2025-06-24 19:40:42 -0300

    Remove arquivos desnecessários após o clone

--- routes/contatoRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ContatoController = require('../controllers/contatoController');
-
-router.post('/api/contato', ContatoController.enviarMensagem);
-
-module.exports = router;
--- routes/ongsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ongsController = require('../controllers/ongsController');
-
-router.post('/cadastrar', ongsController.cadastrar);
-router.get('/', ongsController.listar);
-
-module.exports = router;
